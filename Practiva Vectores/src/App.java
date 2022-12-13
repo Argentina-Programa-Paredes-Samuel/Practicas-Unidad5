34b063b8d23b257551a627cdd1de3840b609788f
Samas1503 <samueleliasparedes.10@gmail.com>
2022-12-13 20:00:39 -0300

    correccion en archivo principal

--- Practiva Vectores/src/App.java
+++ Practiva Vectores/src/App.java
@@ -1,10 +1,20 @@
+import Vectores.Punto1;
 import Vectores.Punto2;
+import Vectores.Punto3;
+import Vectores.Punto4;
+import Vectores.Punto5;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Punto1 p1 = new Punto1();
-        // p1.principal();
+        Punto1 p1 = new Punto1();
+        p1.principal();
         Punto2 p2 = new Punto2();
         p2.principal();
+        Punto3 p3 = new Punto3();
+        p3.principal();
+        Punto4 p4 = new Punto4();
+        p4.principal();
+        Punto5 p5 = new Punto5();
+        p5.principal();
     }
 }
